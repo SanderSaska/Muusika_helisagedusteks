71a229948ffaf4108e780bb3842129b428e10ff3
SanderSaska <sander.saska@gmail.com>
2021-12-15 19:32:21 +0200

    parandused

--- viisi_võrdleja.py
+++ viisi_võrdleja.py
@@ -1,15 +1,13 @@
 from crepe.core import process_file
 import os
 from pathlib import Path
-from typing import no_type_check_decorator
-import numpy as np
 import matplotlib.pyplot as plt
 
 print('Enne programmi kasutamist on sul vaja alla laadida teek Crepe: https://pypi.org/project/crepe/')
 
 ################# def Funktsioonid
 
-def otsi_asukohta(failinimi, faili_asukoht): # Otsib faili asukoha
+def otsi_asukohta(failinimi, faili_asukoht): # Otsib faili asukoha https://www.geeksforgeeks.org/find-path-to-the-given-file-using-python/
     try:
         for root, dirs, files in os.walk(faili_asukoht):
             for name in files:
@@ -20,7 +18,7 @@ def otsi_asukohta(failinimi, faili_asukoht): # Otsib faili asukoha
     except:
         print('Faili asukoht ei ole õigesti kirjutatud')
 
-def loe_andmeid(failinimi): # Loe failist vajalikud andmed
+def loe_andmeid(failinimi): # Loe failist vajalikud andmed 
     f = open(failinimi, 'r', encoding='utf-8')
     aeg = []
     helisagedus = []
@@ -52,12 +50,12 @@ while True:
 
         try:
             if faili_sihtkoht_1 != None and täpsus > 0 and kaust_1 == '': # Kui uut kausta ei loodud
-                process_file(faili_sihtkoht_1, output=None, model_capacity='full', viterbi=True, center=True, save_activation=True, save_plot=True, plot_voicing=False, step_size=täpsus, verbose=True)
+                process_file(faili_sihtkoht_1, output=None, model_capacity='full', viterbi=True, center=True, save_activation=True, save_plot=True, plot_voicing=False, step_size=täpsus, verbose=True) # https://github.com/marl/crepe/blob/master/crepe/core.py
                 print('Tulemused salvestatud')
             elif faili_sihtkoht_1 != None and täpsus > 0: # Kui loodi uus kaust
                 sihtkoht = str(os.path.join(faili_sihtkoht_1.strip(fail_1), kaust_1))
-                Path(sihtkoht).mkdir(parents=True, exist_ok=True)
-                process_file(faili_sihtkoht_1, output=sihtkoht, model_capacity='full', viterbi=True, center=True, save_activation=True, save_plot=True, plot_voicing=False, step_size=täpsus, verbose=True)
+                Path(sihtkoht).mkdir(parents=True, exist_ok=True) # https://www.geeksforgeeks.org/create-a-directory-in-python/
+                process_file(faili_sihtkoht_1, output=sihtkoht, model_capacity='full', viterbi=True, center=True, save_activation=True, save_plot=True, plot_voicing=False, step_size=täpsus, verbose=True) # https://github.com/marl/crepe/blob/master/crepe/core.py
                 print('Tulemused salvestatud faili ' + kaust_1)
             elif faili_sihtkoht_1 != None and täpsus <= 0: # Kui täpsus on negatiivne
                 print('Ei saanud salvestada, täpsus oli antud negatiivsena')
@@ -80,7 +78,7 @@ while True:
 
             aeg, helisagedus = loe_andmeid(faili_sihtkoht_2_1)
             
-            # Vajalikute andmetega loo graafik
+            # Vajalikute andmetega loob graafiku
             plt.plot(aeg, helisagedus, 'r')
             plt.plot()
             plt.axis([0, round(max(aeg)+5, 1), 0, round(max(helisagedus), 1)])
@@ -101,7 +99,7 @@ while True:
             aeg_1, helisagedus_1 = loe_andmeid(faili_sihtkoht_2_2_1)
             aeg_2, helisagedus_2 = loe_andmeid(faili_sihtkoht_2_2_2)
 
-            # Vajalikute andmetega loo graafik
+            # Vajalikute andmetega loob graafiku
             plt.plot(aeg_1, helisagedus_1, 'r', aeg_2, helisagedus_2, 'b')
             plt.ylabel('Helisagedused')
             plt.xlabel('Aeg')
